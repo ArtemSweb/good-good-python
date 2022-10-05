bbbafbdce2900a3295541fb695889e17b166a242
ArtemSweb <solodovnikov-artem@inbox.ru>
2022-10-05 18:17:32 +0300

    lesson 3.5

---
+++ 3. String and list/3.4/step_6.py
@@ -0,0 +1,5 @@
+"""Используя raw-строки, задайте строку, содержащую этот путь к файлу: C:\WINDOWS\System32\drivers\etc\hosts. Результат отобразите на экране."""
+
+text = 'C:\WINDOWS\System32\drivers\etc\hosts'
+
+print(text)
